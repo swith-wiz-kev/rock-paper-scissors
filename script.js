fc1a3695e6124ddf1e3a93acd8f8a53361e21fd4
kev <ddrain10@gmail.com>
2022-11-16 03:01:00 +0800

    Add fight function

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function getComputerChoice() {
         return "Scissors";
     }   
 }
+
+function fight(a,b) {
+    let playerOutcome = "";
+    if (a == b) {
+        playerOutcome = "Draw! Both chose " + a + ".";
+    } else if ((a == "Rock" && b =="Scissors") || (a == "Scissors" && b =="Paper") || (a == "Paper" && b =="Rock") )  {
+        playerOutcome = "You Win! " + a + " beats " + b + ".";
+    } else {
+        playerOutcome = "You Lose! " + b + " beats " + a + ".";
+    }
+
+    return playerOutcome;
+}
+
+// let playerSelection = prompt("Rock, Paper, Scissors?");
+// while (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors" ) {
+//     playerSelection = prompt("Rock, Paper, Scissors?");
+        // if  (typeof playerSelection == "string") {
+        //     playerSelection= playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        // }
+// }
+
+// let playerSelectionCorrected = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+// let computerSelection = getComputerChoice();
+
+
