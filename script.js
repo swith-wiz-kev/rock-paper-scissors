8d7a7db629a385b79c04facade63dffb2eb2374a
kev <ddrain10@gmail.com>
2022-11-16 02:20:23 +0800

    Add opponent choice randomizer

---
+++ script.js
@@ -0,0 +1,11 @@
+// @ts-check
+function getComputerChoice() {
+    const choice = Math.floor(Math.random()*3);
+    if (choice == 0) {
+        return "Rock";
+    } else if (choice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }   
+}
